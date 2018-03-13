e94def8e4b67566f72136ebc83c4b0568eb41029
Sam Stiffman <samstiffman@gmail.com>
2018-03-13 11:25:29 -0500

    Update MyDouble.java

--- MyDouble.java
+++ MyDouble.java
@@ -6,7 +6,7 @@ public class MyDouble extends MyNumber{
 	 */
 	private static final long serialVersionUID = 1L;
 	
-	private double value;
+	private final double value;
 	
 	public MyDouble(double val) {
 		value = val;
