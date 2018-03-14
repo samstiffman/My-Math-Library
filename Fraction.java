bce0e5ebde18395baa82605953768f521b44c817
Sam Stiffman <samstiffman@gmail.com>
2018-03-14 12:44:27 -0500

    Update Fraction.java

--- Fraction.java
+++ Fraction.java
@@ -25,7 +25,6 @@ class Fraction extends MyNumber{
 			reduce();
 		}
 	}
-	
 	/**
 	 * 1 Number long cunstructor for a fractional representation of an Integer
 	 * @param num
@@ -37,32 +36,15 @@ class Fraction extends MyNumber{
 	/**
 	 * Fraction constructor for converting double -> fraction
 	 */
-	public Fraction(double a) {
-		if(a != 0) {
-			a*=1000;
-			numerator = (int) Math.floor(a);
+	public Fraction(double input) {
+		if(input != 0) {
+			input*=1000;
+			numerator = (int) Math.floor(input);
 			denominator = 1000;
 			reduce();
 		}else {
 			numerator = 0;
 			denominator = 1;
-		}
-	}
-	/**
-	 * Reduces the fraction used when a new fraction is made
-	 */
-	private void reduce() {
-		if(numerator%denominator == 0) {
-			numerator = numerator / denominator;
-			denominator = 1;
-			return;
-		}
-		long greater = (numerator>denominator)? numerator : denominator;
-		for(int i=2; i<greater; i++) {
-			while(numerator%i == 0 && denominator%i == 0) {
-				numerator  = numerator / i;
-				denominator /= i;
-			}
 		}
 	}
 	/**
@@ -81,107 +63,184 @@ class Fraction extends MyNumber{
 		}
 		return new Fraction((long)a, temp);
 	}
-	public Fraction multiplyF(Fraction a) throws denominatorZeroException {
-		Fraction newFraction = new Fraction(numerator * a.numerator, denominator * a.denominator);
-		return newFraction;
+	//Base operations for longs
+	public Fraction multiplyNumber(long input) throws denominatorZeroException {
+		return new Fraction(numerator*input, denominator);
 	}
-	public Fraction divideF(Fraction a) throws denominatorZeroException {
-		Fraction newFraction = new Fraction(numerator * a.denominator, denominator * a.numerator);
-		return newFraction;
+	public Fraction divideNumber(long input) throws denominatorZeroException {
+		return new Fraction(numerator, denominator*input);
 	}
-	public Fraction addF(Fraction a) throws denominatorZeroException {
-		long newNum;
-		long newDenom;
-		
-		newNum = (denominator * a.numerator) + (numerator * a.denominator);
-		newDenom = denominator * a.denominator;
-		
-		Fraction newFraction = new Fraction(newNum, newDenom);
-		
-		//The key here is that because we rely on the reduce method when making a new fraction
-		//We don't need to worry about having smallest denominator
-		return newFraction;
+	public Fraction addNumber(long input) throws denominatorZeroException {
+		Fraction temp = new Fraction(input,1);
+		return addF(temp);
 	}
-	public Fraction subtractF(Fraction a) throws denominatorZeroException {
-		long newNum;
-		long newDenom;
-		newNum =  (numerator * a.denominator) - (denominator * a.numerator);
-		newDenom = denominator * a.denominator;
-		Fraction newFraction = new Fraction(newNum, newDenom);
-		
-		//The key here is that because we rely on the reduce method when making a new fraction
-		//We don't need to worry about having smallest denominator
-		return newFraction;
+	public Fraction subtractNumber(long input) throws denominatorZeroException {
+		Fraction temp = new Fraction(input,1);
+		return subtractF(temp);
 	}
-	public Fraction exponentiate(int power) throws denominatorZeroException {
-		long newNum = 1;
-		long newDenom = 1;
-		for(int i=0; i<power; i++) {
-			newNum *= numerator;
-			newDenom *= denominator;
+	//Base operations for doubles
+	public Fraction multiplyNumber(double input) throws denominatorZeroException {
+		Fraction newNumber = rationalize(input);
+		return multiplyF(newNumber);
+	}
+	public Fraction divideNumber(double input) throws denominatorZeroException {
+		Fraction newNumber = rationalize(input);
+		return divideF(newNumber);
+	}
+	public Fraction addNumber(double input) throws denominatorZeroException {
+		Fraction newNumber = rationalize(input);
+		return addF(newNumber);
+	}
+	public Fraction subtractNumber(double input) throws denominatorZeroException {
+		Fraction newNumber = rationalize(input);
+		return subtractF(newNumber);
+	}
+	/**
+	 * Reduces the fraction used when a new fraction is made
+	 */
+	public void reduce() {
+		if(numerator%denominator == 0) {
+			numerator = numerator / denominator;
+			denominator = 1;
+			return;
 		}
-		return new Fraction(newNum,newDenom);
+		long greater = (numerator>denominator)? numerator : denominator;
+		for(int i=2; i<greater; i++) {
+			while(numerator%i == 0 && denominator%i == 0) {
+				numerator  = numerator / i;
+				denominator /= i;
+			}
+		}
 	}
-	public double getValue() {
-		return numerator/denominator;
+	/**
+	 * Simple method for multiplying 2 fractions
+	 * @return Product of fraction*input fraction
+	 */
+	public Fraction multiplyF(Fraction input) {
+		try {
+			Fraction newFraction = new Fraction(numerator * input.numerator, denominator * input.denominator);
+			return newFraction;
+		} catch (denominatorZeroException e) {
+			e.printStackTrace();
+		}
+		return null;
 	}
+	/**
+	 * Simple method for dividing 2 fractions input fraction cannot be 0 or there will be a denominatorZeroException
+	 * @return Product of fraction*1/input fraction
+	 */
+	public Fraction divideF(Fraction input){
+		try {
+			Fraction newFraction = new Fraction(numerator * input.denominator, denominator * input.numerator);
+			return newFraction;
+		} catch (denominatorZeroException e) {
+			e.printStackTrace();
+			return null;
+		}
+	}
+	/**
+	 * Method for adding 2 fractions
+	 * @return Sum of fraction +input fraction
+	 */
+	public Fraction addF(Fraction input){
+		try {
+			long newNum = (denominator * input.numerator) + (numerator * input.denominator);
+			long newDenom = denominator * input.denominator;
+			
+			Fraction newFraction = new Fraction(newNum, newDenom);
+			
+			//The key here is that because we rely on the reduce method when making a new fraction
+			//We don't need to worry about having smallest denominator
+			return newFraction;
+		} catch (denominatorZeroException e) {
+			e.printStackTrace();
+			return null;
+		}
+
+	}
+	/**
+	 * Method for subtracting 2 fractions
+	 * @return Sum of fraction -input fraction
+	 */
+	public Fraction subtractF(Fraction input){
+		try {
+			long newNum =  (numerator * input.denominator) - (denominator * input.numerator);
+			long newDenom = denominator * input.denominator;
+			Fraction newFraction = new Fraction(newNum, newDenom);
+			
+			//The key here is that because we rely on the reduce method when making a new fraction
+			//We don't need to worry about having smallest denominator
+			return newFraction;
+		} catch (denominatorZeroException e) {
+			e.printStackTrace();
+			return null;
+		}
+	}
+	/**
+	 * Takes the fraction and exponentiates it to the correct power
+	 * @param power
+	 * @return this fraction^power
+	 */
+	public Fraction exponentiate(int power){
+		if(power == 1) 
+			return this;
+		try {
+			if(power<0) {
+				long newNum = 1;
+				long newDenom = 1;
+				for(int i=0; i<power; i++) {
+					newNum *= denominator;
+					newDenom *= numerator;
+				}
+				return new Fraction(newNum, newDenom);
+			}
+			else if(power==0) 
+				return new Fraction(1,1);
+			long newNum = 1;
+			long newDenom = 1;
+			for(int i=0; i<power; i++) {
+				newNum *= numerator;
+				newDenom *= denominator;
+			}
+			return new Fraction(newNum,newDenom);
+		} catch (denominatorZeroException e) {
+			e.printStackTrace();
+			return null;
+		}
+	}
+	
 	public String toString() {
 		if(denominator == 1 || numerator == 0)
 			return String.valueOf(numerator);
 		else
 			return  numerator + "/" + denominator;
 	}
-		//Base operations for longs
-	public Fraction multiplyNumber(long a) throws denominatorZeroException {
-		return new Fraction(numerator*a, denominator);
-	}
-	public Fraction divideNumber(long a) throws denominatorZeroException {
-		return new Fraction(numerator, denominator*a);
-	}
-	public Fraction addNumber(long a) throws denominatorZeroException {
-		Fraction temp = new Fraction(a,1);
-		return addF(temp);
-	}
-	public Fraction subtractNumber(long a) throws denominatorZeroException {
-		Fraction temp = new Fraction(a,1);
-		return subtractF(temp);
-	}
-	//Base operations for doubles
-	public Fraction multiplyNumber(double d) throws denominatorZeroException {
-		Fraction newNumber = rationalize(d);
-		return multiplyF(newNumber);
-	}
-	public Fraction divideNumber(double d) throws denominatorZeroException {
-		Fraction newNumber = rationalize(d);
-		return divideF(newNumber);
-	}
-	public Fraction addNumber(double d) throws denominatorZeroException {
-		Fraction newNumber = rationalize(d);
-		return addF(newNumber);
-	}
-	public Fraction subtractNumber(double d) throws denominatorZeroException {
-		Fraction newNumber = rationalize(d);
-		return subtractF(newNumber);
-	}
 	/**
 	 * Square Root function for fractions
 	 * @param input
-	 * @return
-	 * @throws denominatorZeroException 
+	 * @return Square root of the input fraction
 	 */
-	public SquareRoot exactSquareRoot() throws denominatorZeroException {
-		
-		SquareRoot num = ExactMath.exactSquareRoot(numerator);
-		SquareRoot denom = ExactMath.exactSquareRoot(denominator);
-		Fraction out = num.outsidePart.divideF(denom.outsidePart);
-		Fraction in = num.insidePart.divideF(denom.insidePart);
-		
-		return new SquareRoot(in, out);
+	public SquareRoot exactSquareRoot(){
+		try {
+			SquareRoot num = ExactMath.exactSquareRoot(numerator);
+			SquareRoot denom = ExactMath.exactSquareRoot(denominator);
+			Fraction out = num.outsidePart.divideF(denom.outsidePart);
+			Fraction in = num.insidePart.divideF(denom.insidePart);
+			
+			return new SquareRoot(in, out);
+		} catch (denominatorZeroException e) {
+			e.printStackTrace();
+		}
+		return null;		
 	}
-	public CubeRoot exactCubeRoot() throws denominatorZeroException {
+	/**
+	 * Cube Root function for fractions
+	 * @param input
+	 * @return Cube root of the input fraction
+	 */
+	public CubeRoot exactCubeRoot(){
 		CubeRoot num = ExactMath.exactCubeRoot(numerator);
 		CubeRoot denom = ExactMath.exactCubeRoot(denominator);
-		
 		Fraction out = num.outsidePart.divideF(denom.outsidePart);
 		Fraction in = num.insidePart.divideF(denom.insidePart);
 		
@@ -193,7 +252,9 @@ class Fraction extends MyNumber{
 	public long getDenominator() {
 		return denominator;
 	}
-
+	public double getValue() {
+		return numerator/denominator;
+	}
 	class denominatorZeroException extends Exception {
 
 		/**
@@ -212,4 +273,5 @@ class Fraction extends MyNumber{
 		}
 
 	}
+	
 }
